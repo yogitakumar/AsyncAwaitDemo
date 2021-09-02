4772b0329583b47e387660b95073f803bef3813d
Yogita Kumar <61360028+yogitakumar@users.noreply.github.com>
2021-09-02 13:52:47 +0100

    Add files via upload

---
+++ AsyncAwaitDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SayHello();
+            SayBye();
+        }
+
+        public static async Task SayHello()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Hello");
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+
+        public static void SayBye()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Bye");
+                Task.Delay(100).Wait();
+            }
+        }
+    }
+}
